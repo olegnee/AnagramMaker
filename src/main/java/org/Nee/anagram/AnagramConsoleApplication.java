b50d33686ce98eec09e59eb26ca5318e3a83f455
Oleg.Nee <89423263+olegnee@users.noreply.github.com>
2022-12-23 04:17:20 +0900

    AnagramCreator added

---
+++ src/main/java/org/Nee/anagram/AnagramConsoleApplication.java
@@ -0,0 +1,9 @@
+package org.Nee.anagram;
+
+public class AnagramConsoleApplication
+{
+    public static void main( String[] args ) {
+        AnagramCreator anagramCreator = new AnagramCreator();
+        System.out.println(anagramCreator.makeAnagram("som1e wo3rds"));
+    }
+}
